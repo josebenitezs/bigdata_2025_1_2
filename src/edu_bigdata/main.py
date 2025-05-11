6873d04f43ede40958219a11a5288a35c3ae3e77
josebenitezs <113186263+josebenitezs@users.noreply.github.com>
2025-05-11 16:46:17 +0000

    creación de la clase database

--- src/edu_bigdata/main.py
+++ src/edu_bigdata/main.py
@@ -1,5 +1,5 @@
 from dataweb import DataWeb
-#from database import DataBase
+from database import DataBase
 import pandas as pd
 
 
@@ -7,7 +7,7 @@ import pandas as pd
 def main():
     #df = pd.DataFrame()
     dataweb = DataWeb()
-    #database = DataBase()
+    database = DataBase()
     df = dataweb.obtener_datos()
     df = dataweb.convertir_numericos(df)
     print("*************** imprecion de los datos obtenidos ************************")
@@ -15,6 +15,7 @@ def main():
     print(df.head())
     df.to_csv("src/edu_bigdata/static/csv/data_web.csv", index=False) #/workspaces/bigdata_2025_1_2/src/edu_bigdata/static/csv
     #nombre_tabla = "dolar_analisis"
+    database.close.database()
     #database.insert_data(df,nombre_tabla)
     #print("*************** Insertar los datos obtenidos en la base datos tabla: {}*********".format(nombre_tabla))
     #print(df.shape)
