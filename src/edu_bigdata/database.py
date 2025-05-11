a1c5df0acac5bba1f25af7cec33a92f1da1093e4
josebenitezs <113186263+josebenitezs@users.noreply.github.com>
2025-05-11 19:00:41 +0000

    configuracion de DB

--- src/edu_bigdata/database.py
+++ src/edu_bigdata/database.py
@@ -8,19 +8,7 @@ class DataBase:
     def __init__(self):
         self.db_name = "src/edu_bigdata/static/db/dolar_analisis.sqlite" #/workspaces/bigdata_2025_1_2/src/edu_bigdata/static/db
 
-        self.create_database()
-
-    def create_database(self):
-        try:
-            self.conn = sqlite3.connect(self.db_name)
-            self.cursor = self.conn.cursor() 
-        
-        except Exception as err:
-            print("error al crear la base de datos")
-
-        def close_database(self):
-            self.conn.close()
-
+    
 
 
 
--- src/edu_bigdata/main.py
+++ src/edu_bigdata/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 def main():
-    #df = pd.DataFrame()
+    df = pd.DataFrame()
     dataweb = DataWeb()
     database = DataBase()
     df = dataweb.obtener_datos()
@@ -14,15 +14,14 @@ def main():
     print(df.shape)
     print(df.head())
     df.to_csv("src/edu_bigdata/static/csv/data_web.csv", index=False) #/workspaces/bigdata_2025_1_2/src/edu_bigdata/static/csv
-    #nombre_tabla = "dolar_analisis"
-    database.close.database()
-    #database.insert_data(df,nombre_tabla)
-    #print("*************** Insertar los datos obtenidos en la base datos tabla: {}*********".format(nombre_tabla))
-    #print(df.shape)
-    #print(df.head())
-    #df_2 = database.read_data(nombre_tabla)
-    #print(df_2.shape)
-    #print(df_2.head())
+    nombre_tabla = "dolar_analisis"
+    database.insert_data(df,nombre_tabla)
+    print("*************** Insertar los datos obtenidos en la base datos tabla: {}*********".format(nombre_tabla))
+    print(df.shape)
+    print(df.head())
+    df_2 = database.read_data(nombre_tabla)
+    print(df_2.shape)
+    print(df_2.head())
 
 if __name__ == "__main__":
     main()
