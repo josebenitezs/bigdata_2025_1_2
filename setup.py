196ee0a15ccd12152509e12d1e1af769be563ff3
josebenitezs <113186263+josebenitezs@users.noreply.github.com>
2025-05-11 15:04:14 +0000

    configuracion dependencias

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="edu_bidata",
+    author="Jose Benitez",
+    author_email="jose.benitez@est.iudigital.edu.co",
+    description="",
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests",
+        "BeautifulSoup4"
+    ] 
+)
